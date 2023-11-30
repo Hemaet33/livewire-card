d1617314e9b514b03abcfb95bb385346b0f80712
hemaet <hemaet33@gmail.com>
2023-11-30 16:48:02 +0600

    designing full card

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      boxShadow:{
+        card:"0 -1px 2px #312f2f,0px 2px 1px #0b101c"
+      },
+    },
+  },
+  plugins: [],
+}
+
